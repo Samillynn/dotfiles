b36a421e16ca72c64869404aa0c67c00be624df0
Samill <2837931428@qq.com>
2020-08-07 21:38:07 +0800

    add linkall.sh ( a shell script to link files)

---
+++ linkall.sh
@@ -0,0 +1,6 @@
+ln -s .functions $HOME/.functions
+ln -s .aliases $HOME/.aliases
+ln -s .nvimrc $HOME/.nvimrc
+ln -s .zshrc $HOME/.zshrc
+ln -s .tmux.conf $HOME/.tmux.conf
+ln -s .tmux.conf.local $HOME/.tmux.conf.local
